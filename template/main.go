29bc8b1234a4c82211cefb73a9821405e9746140
HeadTriXz <l.knol@outlook.com>
2023-12-14 09:56:04 +0100

    chore: add template

---
+++ template/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	_ "embed"
+	"log"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+func getInputLines() []string {
+	return strings.Split(strings.TrimRight(input, "\n"), "\n")
+}
+
+func part1() int {
+	return 0
+}
+
+func part2() int {
+	return 0
+}
+
+func main() {
+	part1Result := part1()
+	part2Result := part2()
+
+	log.Printf("Part 1: %d", part1Result)
+	log.Printf("Part 2: %d", part2Result)
+}
